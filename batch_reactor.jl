55670bc52d1fadfda6efa7de0ac5692433091bdf
wangli <913832546@qq.com>
2019-10-09 13:21:14 +0200

    optimize the code

--- batch_reactor.jl
+++ batch_reactor.jl
@@ -1,5 +1,5 @@
 # used packages
-using DifferentialEquations, Optim, RecursiveArrayTools, DiffEqParamEstim
+using DifferentialEquations, Optim
 
 #batch reactor A + 2B <-> C, example 6.4 of SAMM
 function batch(du, u, p, t)
@@ -9,14 +9,14 @@ function batch(du, u, p, t)
 end
 
 u0 = [1.0, 2.0, 0.0]
-p = [1.0, 1.0]
+p = [.5, .5]
 tspan = (0.0, 1.0)
 prob = ODEProblem(batch, u0, tspan, p)
 sol = solve(prob, Tsit5())
 
 ## data generation using p=[1.0, 1.0]
-t = range(0, stop=1,length = 200)
-randomized = VectorOfArray([sol(t[i]) + 0.01randn(3) for i in 1:length(t)])
+t = range(0, stop=1,length = 2000)
+randomized = [sol(t[i]) + 0.01randn(3) for i in 1:length(t)]
 
 ## define cost function and optimize
 function solver(para)
@@ -25,6 +25,7 @@ function solver(para)
     return (solution)
 end
 
+# calculate the sum_square
 function sum_sqaure_arr(m)
     tot = 0.0
     for i in 1:length(m)
@@ -46,7 +47,6 @@ function loss(para)
     return (L)
 end
 
-#cost_function = build_loss_objective(prob, Tsit5(), L2Loss(t,data,differ_weight=0.3, data_weight=0.7),
-#                mixiters = 10000, verbose = false)
+# optimize
 x0 = [0.0, 0.0]
 res = Optim.optimize(loss, x0)
