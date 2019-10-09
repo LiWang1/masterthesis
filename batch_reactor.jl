f38b90ce6e200cbc64b9232e2d8ad099bba0788c
wangli <913832546@qq.com>
2019-10-09 09:20:48 +0200

    re-write cost functions

--- batch_reactor.jl
+++ batch_reactor.jl
@@ -15,12 +15,38 @@ prob = ODEProblem(batch, u0, tspan, p)
 sol = solve(prob, Tsit5())
 
 ## data generation using p=[1.0, 1.0]
-t = collect(range(0, stop=1,length = 200))
+t = range(0, stop=1,length = 200)
 randomized = VectorOfArray([sol(t[i]) + 0.01randn(3) for i in 1:length(t)])
-data = convert(Array, randomized)
 
 ## define cost function and optimize
-cost_function = build_loss_objective(prob, Tsit5(), L2Loss(t,data,differ_weight=0.3, data_weight=0.7),
-                mixiters = 10000, verbose = false)
+function solver(para)
+    problem = ODEProblem(batch, u0, tspan, para)
+    solution = solve(problem, Tsit5())
+    return (solution)
+end
+
+function sum_sqaure_arr(m)
+    tot = 0.0
+    for i in 1:length(m)
+        tot += m[i]^2
+    end
+    return (tot)
+end
+
+
+# cost function
+function loss(para)
+    ## solve model with the new parameter
+    solution = solver(para)
+    ## run model at time
+    L = 0.0
+    for i in 1:length(t)
+        L += sum_sqaure_arr(solution(t[i]) - randomized[i])
+    end
+    return (L)
+end
+
+#cost_function = build_loss_objective(prob, Tsit5(), L2Loss(t,data,differ_weight=0.3, data_weight=0.7),
+#                mixiters = 10000, verbose = false)
 x0 = [0.0, 0.0]
-res = Optim.optimize(cost_function, x0, Optim.BFGS())
+res = Optim.optimize(loss, x0)
