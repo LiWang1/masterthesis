86c212868995ff4ab7612c9b26f10c30b26e96ff
wangli <913832546@qq.com>
2019-10-16 15:39:35 +0200

    'pass gradient to optimizer'

--- gradient.jl
+++ gradient.jl
@@ -1,11 +1,13 @@
 # forward AD
 using ForwardDiff
-f(x::Vector) = x[1] + 10*x[2].^2
-x = [1, 2]
-#gradient = ForwardDiff.gradient(f, x)
-hessian = ForwardDiff.hessian(f, x)
-# reverse AD
-using Zygote
-m(x::Vector) = x[1] + 10*x[2].^2
-gradient2 = Zygote.gradient(m, x)
-#hessian2 = Zygote.hessian(m, x)
+using Optim
+f(x::Vector) = (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
+
+function grad!(G, m)
+    grad = ForwardDiff.gradient(f, m)
+    G[1] = grad[1]
+    G[2] = grad[2]
+end
+
+initial_x = [0.0, 0.0]
+res = Optim.minimizer(optimize(f, grad!, initial_x, BFGS()))
