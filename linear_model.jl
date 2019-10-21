1f56f31954ddaf528f19fba8f54a02f906ebb8a4
wangli <913832546@qq.com>
2019-10-21 14:50:21 +0200

    "scale the linear model"

--- linear_model.jl
+++ linear_model.jl
@@ -2,18 +2,21 @@
 # Mail: wangli@student.ethz.ch
 #
 #
-# --------------------------------------
+# --------------------------------------------------------------
 using Pkg
 Pkg.activate(".")
 
 using Optim, ForwardDiff
 
+
 # ------------
 # 1) define true model and data generation
-function y(x)
-    return y = x[1]+2*x[2]+3*x[3]
-end
-x_obs = randn(2000, 3)
+# 1.1) define the number of dimensions
+num_true_para = 100
+true_para = 1:num_true_para
+# 1.2) data generation for the parameters defined
+y(x::Vector) = transpose(true_para)*x
+x_obs = randn(2000, num_true_para)
 y_obs = [y(x_obs[i,:]) + randn() for i in 1:2000]
 
 # 2) define the linear model
@@ -32,14 +35,14 @@ end
 # 4) define the way to calculate the gradients for the cost function
 function grad!(G, para)
     grad = ForwardDiff.gradient(loss, para)
-    G[1] = grad[1]
-    G[2] = grad[2]
-    G[3] = grad[3]
+    for i in 1:num_true_para
+        G[i] = grad[i]
+    end
 end
 
 
 # 5) optimize the loss function with diffferent method
-x0 = [0.0, 0.0, 0.0]
+x0 = zeros(num_true_para)
 # without gradient
 #res1 = optimize(loss, x0)
 # autodiff
