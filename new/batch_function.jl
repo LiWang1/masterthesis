cb1f2b9551be67201815121acf9c2634c97c6bef
wangli <913832546@qq.com>
2020-02-07 17:09:05 +0100

    add replacement of dwp

--- new/batch_function.jl
+++ new/batch_function.jl
@@ -1,11 +1,16 @@
+
 # batch train on the training data sets
 function batchTrain(obs_data, Rain, Dry, ann, u_update, tspan, ts, n_itr, ps, fig)
     # obs_data: data sequence for training
+    # Dry: dry weather flow data, if Dry == -1, then the dry weather pattern
+    #      is replaced, otherwise the infiltration is replaced with NN
     # ann: neural network that's used for prediction
     # u_update: updated u0 for the next batch training
     # tspan: the time span of each trainning batch
     # ts: time step for the result of the ode solver
     # n_itr: number of iteration for each batch data
+    # ps: the characteristic parameters of the catchments
+    # fig: if fig==1, the visualization is showed
 
     p1,re = Flux.destructure(ann)
     # p1: paramters of the NN
@@ -13,13 +18,74 @@ function batchTrain(obs_data, Rain, Dry, ann, u_update, tspan, ts, n_itr, ps, fi
 
     num_batch = length(obs_data)
     for i in 1:num_batch
-        u_update, p1 = russikon_nn(obs_data[i], Rain[i], Dry[i], u_update, p1, re, tspan, ts, n_itr, ps, fig)
+        if Dry!= -1  # if the Dry weather data is provided
+          u_update, p1 = russikon_nn(obs_data[i], Rain[i], Dry[i], u_update, p1, re, tspan, ts, n_itr, ps, fig)
+        elseif Dry == -1
+          u_update, p1 = russikon_nn_(obs_data[i], Rain[i], u_update, p1, re, tspan, ts, n_itr, ps, fig)
+        end
     end
     # return the final trained parameters
     return u_update, p1
 end
 
-# training of one batch
+# create the batches for rain data
+function batch_sep_rain(data, start, endt, num, span)
+  # data: data set
+  # num: number of batches
+  # span: time span for each batch
+  result = Dict()
+  if num == 1
+    rain_index = findall(x->(x<=endt && x>= start), data.int_date)
+    rain_batch_data = data[rain_index,:]
+    rain_batch_data.mins = rain_batch_data.mins_hour.+1440*(rain_batch_data.int_date.-start)
+    rain_interp = LinearInterpolation(rain_batch_data.mins, rain_batch_data.V3)
+    interp_rain(x) = convert(Float32, rain_interp(x))
+    return interp_rain
+  elseif num>1
+    for i in 1:num
+      start_ = addDays(start, span*(i-1))
+      end_ = addDays(endt, span*(i-1))
+      rain_index_ = findall(x->(x<=end_ && x>= start_), data.int_date)
+      rain_batch_data_ = data[rain_index_,:]
+      rain_batch_data_.mins = rain_batch_data_.mins_hour.+1440*(rain_batch_data_.int_date.-start_)
+      rain_interp_ = LinearInterpolation(rain_batch_data_.mins, rain_batch_data_.V3)
+      interp_rain_(x) = convert(Float32, rain_interp_(x))
+      result[i] = interp_rain_
+    end
+    return result
+  end
+end
+
+# create the batches for carry on flow data
+function batch_sep_flow(data, start, endt, num, span, ts)
+  # data: data set
+  # num: number of batches
+  # span: time span for each batch
+  # ts: time steps for evaluation
+  result = Dict()
+  if num == 1
+    flow_index = findall(x->(x<=endt && x>= start), data.int_date)
+    flow_batch_data = data[flow_index,:]
+    flow_batch_data.mins = flow_batch_data.mins_hour.+1440*(flow_batch_data.int_date.-start)
+    flow_interp = LinearInterpolation(flow_batch_data.mins, flow_batch_data.V3)
+    interp_flow = convert(Array{Float32, 1}, flow_interp(ts))
+    return interp_flow
+  elseif num>1
+    for i in 1:num
+      start_ = addDays(start, span*(i-1))
+      end_ = addDays(endt, span*(i-1))
+      flow_index_ = findall(x->(x<=end_ && x>= start_), data.int_date)
+      flow_batch_data_ = data[flow_index_,:]
+      flow_batch_data_.mins = flow_batch_data_.mins_hour.+1440*(flow_batch_data_.int_date.-start_)
+      flow_interp_ = LinearInterpolation(flow_batch_data_.mins, flow_batch_data_.V3)
+      interp_flow_ = convert(Array{Float32, 1}, flow_interp_(ts))
+      result[i] = interp_flow_
+    end
+    return result
+  end
+end
+
+# training of one batch: replacing the infiltration
 function russikon_nn(carryonFlow, rain, dry, u0, p_ann, re, tspan, ts, n_itr, ps, fig)
   params = Flux.params(p_ann, u0)
   num_para = length(p_ann)
@@ -81,8 +147,72 @@ function russikon_nn(carryonFlow, rain, dry, u0, p_ann, re, tspan, ts, n_itr, ps
   # update
   u_update = predict_n()[end]             #update u, note p1 is somehow automatically updated by Flux.train
   return u_update, p_ann
-
 end
+
+# training of one batch: replacing the dry weather pattern
+function russikon_nn_(carryonFlow, rain, u0, p_ann, re, tspan, ts, n_itr, ps, fig)
+  params = Flux.params(p_ann, u0)
+  num_para = length(p_ann)
+
+  # model
+  function russikon_mix(du, u, p, t)
+    k = ps[1:5]  # k: storage constant
+    theta = ps[6:10]  # theta: runoff coefficient
+    area = ps[11:15]  # area m^2
+    areaRatio = ps[16:20]
+    pt = ps[21:25]  # people eqivalent E
+    infiltration = ps[26]
+    neural = 2f0*142f0/86400f0*re(p[1:num_para])(Float32[(floor(t%1440/60)-12)/12, ((floor(t/1440)+1)-3.5)/3.5])[1]
+    # B
+    du[1] = (theta[1] * area[1] * rain(t) + pt[1]*neural- u[1] + infiltration)/k[1]
+    # C
+    du[2] = (theta[2] * area[2] * rain(t) + pt[2]*neural - u[2] + infiltration)/k[2]
+    # D
+    du[3] = (theta[3] * area[3]* rain(t) + pt[3]*neural - u[3] + infiltration)/k[3]
+    # E
+    du[4] = (theta[4] * area[4] * rain(t) + pt[4]*neural - u[4] + infiltration)/k[4]
+    # F
+    du[5] = (theta[5] * area[5] * rain(t) + pt[5]*neural - u[5] + infiltration)/k[5]
+  end
+
+  prob_russikon_mix = ODEProblem(russikon_mix, u0, tspan, p_ann)
+
+  # prediction
+  function predict_n()
+    #concrete_solve(prob_,Tsit5(),u_update, p1,saveat=ts; sensealg=TrackerAdjoint())
+    concrete_solve(prob_russikon_mix,Tsit5(),u0, p_ann, saveat=ts; sensealg=TrackerAdjoint())
+  end
+  # add
+  add() = addOverflow(predict_n())
+  # loss function
+  loss_russikon() = mean(abs2, carryonFlow - add())
+
+  # training
+  data = Iterators.repeated((), n_itr)    #number of iterations
+  opt = ADAM(0.02)                        #algorithm
+  cb = function()
+    display(loss_russikon())
+    # plot current prediction against obs data
+    pred_nn = add()
+    pl = plot(ts, carryonFlow, label = "obs")
+    plot!(pl, ts, pred_nn, label = "pred")
+    xlabel!("time_min")
+    ylabel!("flow_L/s")
+    display(pl)
+  end
+
+  if fig==1
+    cb()
+    Flux.train!(loss_russikon, params, data, opt, cb = cb)
+    #cb()
+  else
+    Flux.train!(loss_russikon, params, data, opt)
+  end
+  # update
+  u_update = predict_n()[end]             #update u, note p1 is somehow automatically updated by Flux.train
+  return u_update, p_ann
+end
+
 
 # evaluation of the model on test data sets
 function evaluation(carryon, rain, dry, u_update, p1, re, tspan, ts, ps)
@@ -169,60 +299,6 @@ function addOverflow(flow)
   return (out_total)
 end
 
-function batch_sep_rain(data, start, endt, num, span)
-  # data: data set
-  # num: number of batches
-  # span: time span for each batch
-  result = Dict()
-  if num == 1
-    index = findall(x->(x<=endt && x>= start), data.int_date)
-    batch_data = data[index,:]
-    batch_data.mins = batch_data.mins_hour.+1440*(batch_data.int_date.-start)
-    interp = LinearInterpolation(batch_data.mins, batch_data.V3)
-    interp_data(x) = convert(Float32, interp(x))
-    return interp_data
-  end
-
-  for i in 1:num
-    start_ = addDays(start, span*(i-1))
-    end_ = addDays(endt, span*(i-1))
-    index = findall(x->(x<=end_ && x>= start_), data.int_date)
-    batch_data = data[index,:]
-    batch_data.mins = batch_data.mins_hour.+1440*(batch_data.int_date.-start_)
-    interp = LinearInterpolation(batch_data.mins, batch_data.V3)
-    interp_data(x) = convert(Float32, interp(x))
-    result[i] = interp_data
-  end
-  return result
-end
-
-function batch_sep_flow(data, start, endt, num, span, ts)
-  # data: data set
-  # num: number of batches
-  # span: time span for each batch
-  # ts: time steps for evaluation
-  result = Dict()
-  if num == 1
-    index = findall(x->(x<=endt && x>= start), data.int_date)
-    batch_data = data[index,:]
-    batch_data.mins = batch_data.mins_hour.+1440*(batch_data.int_date.-start)
-    interp = LinearInterpolation(batch_data.mins, batch_data.V3)
-    interp_data = convert(Array{Float32, 1}, interp(ts))
-    return interp_data
-  end
-  for i in 1:num
-    start_ = addDays(start, span*(i-1))
-    end_ = addDays(endt, span*(i-1))
-    index = findall(x->(x<=end_ && x>= start_), data.int_date)
-    batch_data = data[index,:]
-    batch_data.mins = batch_data.mins_hour.+1440*(batch_data.int_date.-start_)
-    interp = LinearInterpolation(batch_data.mins, batch_data.V3)
-    interp_data = convert(Array{Float32, 1}, interp(ts))
-    result[i] = interp_data
-  end
-  return result
-end
-
 # add days to dates
 function addDays(date, diff)
   # change to Date form
@@ -236,7 +312,7 @@ end
 function dry(data, num)
   result = Dict()
   interp = LinearInterpolation(data.mins, data.L_mins)
-  dry(x) = convert(Float32, interp(x%1440)/60)
+  dry(x) = convert(Float32, interp(x%1440)/60) # 60: L/min -> L/s
   if num==1
     return dry
   end
--- new/batch_main.jl
+++ new/batch_main.jl
@@ -19,18 +19,19 @@ using Statistics
 using Dates
 
 
+
 include("batch_function.jl")
 
 # model parameters
-p_ori = Float32[       30.0;  30.0;  30.0;  30.0;  30.0;  # k: storage constant
-                       0.31;  0.32;  0.35;  0.36;  0.36;  # theta: runoff coefficient
+p_ori = Float32[       50.0;  50.0;  50.0;  50.0;  50.0;  # k: storage constant
+                       0.25;  0.25;  0.25;  0.25;  0.25;  # theta: runoff coefficient
                      186000; 62500; 66500; 44900;155800;  # area m^2
                        0.36;  0.12;  0.13;  0.09;   0.3;  # area ratio
                         650;   366;   898;   258;  1339;  # people eqivalent E
                         3]                                # infiltration L/s
 
 # time span and time steps
-tspan_end = 11515f0
+tspan_end = 11515f0  # bug here...
 tspan = (0.0f0, tspan_end)
 t = range(tspan[1], tspan[2], length = convert(Int64, floor(tspan_end)))
 
@@ -44,39 +45,50 @@ dwp = CSV.read("/Users/wangli/Desktop/masterthesis/simdata/dwp_L_min_E.csv")
 
 # data batches
 # training
-date_start= 20170601
-date_end = 20170608
+date_start_train= 20170601
+date_end_train = 20170608
 batch_number = 2
 batch_span = 8       # 8 days of data
 
-flow_train = batch_sep_flow(f_03, date_start, date_end, batch_number, batch_span, t)
-rain_train = batch_sep_rain(r_03, date_start, date_end, batch_number, batch_span)
+flow_train = batch_sep_flow(f_03, date_start_train, date_end_train, batch_number, batch_span, t)
+rain_train = batch_sep_rain(r_03, date_start_train, date_end_train, batch_number, batch_span)
 dwp_train = dry(dwp, batch_number)
 
-# testing
-date_start_test= 20170617
-date_end_test = 20170624
 
-flow_test = batch_sep_flow(f_03, date_start, date_end, 1, batch_span, t)
-rain_test = batch_sep_rain(r_03, date_start, date_end, 1, batch_span)
+
+# testing
+date_start_test= 20170609
+date_end_test = 20170616
+
+flow_test = batch_sep_flow(f_03, date_start_test, date_end_test, 1, batch_span, t)
+rain_test = batch_sep_rain(r_03, date_start_test, date_end_test, 1, batch_span)
 dwp_test = dry(dwp, 1)
 
 # paras for NN
 ann = Chain(Dense(2, 10, σ), Dense(10, 10, σ), Dense(10, 1))
+ann2 = Chain(Dense(2, 10, relu), Dense(10, 10, tanh),Dense(10, 10, tanh), Dense(10, 2, tanh), Dense(2, 1))
 p1,re = Flux.destructure(ann)
 n_itr = 10  # number of iteration for each batch
 fig = 1  # fig == 1: visualize the training result
 
 # train the model with neural network
-russikon_nn(carryon1, rain1, dry, u_update, p1, re, tspan, t, n_itr, p_ori, fig)
+# replace the infiltration
+russikon_nn(flow_train[1], rain_train[1], dwp_train[1], u_update, p1, re, tspan, t, n_itr, p_ori, fig)
+# replace the dry weather flow
+russikon_nn_(flow_test, rain_test, u_update, p1, re, tspan, t, n_itr, p_ori, fig)
 
 # train the model with more data (minibatch)
-u, para = batchTrain(Flow_train, Rain_train, Dry_train, ann, u_update, tspan, t, n_itr, p_ori, fig) # should pass the rain data and dry flow inside
+
+u, para = batchTrain(flow_train, rain_train, dwp_train, ann, u_update, tspan, t, n_itr, p_ori, fig) # should pass the rain data and dry flow inside
+# replace the dry weather pattern
+u, para = batchTrain(flow_train, rain_train, -1, ann, u_update, tspan, t, n_itr, p_ori, fig) # should pass the rain data and dry flow inside
 
 # evaluation of the model
-evaluation(carryon3, rain3, dry, u, para, re, tspan, t, p_ori)
+# replace the infiltration
+evaluation(flow_test, rain_test, dwp_test, u, para, re, tspan, t, p_ori)
 
 # performance of the original model
-russikon_original(carryon1, rain1, dry, u_update, tspan, t, p_ori)
+russikon_original(flow_test, rain_test, dwp_test, u_update, tspan, t, p_ori)
+
 
 # visualization and comparison
